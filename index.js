b7c689337673c2089ec0bff872bddad689fc0267
Eleonora Demikh <deademikh@gmail.com>
2023-11-22 00:40:07 +0200

    fixed server

---
+++ index.js
@@ -0,0 +1,27 @@
+const ws = require('ws');
+
+const PORT = process.env.PORT || 5000;
+
+const wss = new ws.Server({ port: PORT });
+
+wss.on('connection', (ws) => {
+  console.log('New person connected');
+
+  ws.on('message', (message) => {
+    message = JSON.parse(message);
+
+    wss.clients.forEach((client) => {
+      client.send(JSON.stringify(message));
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Person was disconnected');
+  });
+});
+
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
+});
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*.{html,js}'];
-export const theme = {
-  fontFamily: {
-    'space': '"Space-Grotesk", sans-serif'
-  },
-  extend: {},
-};
-export const plugins = [];
