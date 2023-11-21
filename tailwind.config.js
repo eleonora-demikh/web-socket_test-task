30a17399c08031e8026958c4aa8f3d5b3ffcb90d
Eleonora Demikh <deademikh@gmail.com>
2023-11-22 00:15:52 +0200

    cteate frontend part

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/**/*.{html,js}'];
+export const theme = {
+  fontFamily: {
+    'space': '"Space-Grotesk", sans-serif'
+  },
+  extend: {},
+};
+export const plugins = [];
